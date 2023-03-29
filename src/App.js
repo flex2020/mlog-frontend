121157fdf629c35b9e3cb09bbb5bc8e6289bb422
NOH UIBIN <hosan_1@naver.com>
2023-03-29 21:50:55 +0900

    Spring boot 연동 테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/post')
+        .then(response => {
+          setPosts(response.data);
+          console.log(response.data);
+        })
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+          {posts.map((post) => {
+            return (
+              <div>
+                <div>{post.title}</div>
+                <div>{post.content}</div>
+                <div>{post.category}</div>
+                <div>{post.postedDate}</div>
+                <div>{post.replyCount}</div>
+              </div>
+            )
+          })}
+        </div>
+    );
 }
 
 export default App;
