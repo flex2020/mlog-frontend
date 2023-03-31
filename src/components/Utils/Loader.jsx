c98a1cd45028e54d4604f87ac87eaee660fc22e5
NOH UIBIN <hosan_1@naver.com>
2023-04-01 01:06:25 +0900

    feat: Loader 컴포넌트 작성

---
+++ src/components/Utils/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/components/Utils/Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
