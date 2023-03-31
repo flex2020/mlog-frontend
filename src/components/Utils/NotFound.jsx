0f667f76f672618bd67d8616609998deee041cd2
NOH UIBIN <hosan_1@naver.com>
2023-04-01 01:06:40 +0900

    feat: NotFound 컴포넌트 작성

---
+++ src/components/Utils/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/components/Utils/NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div class="not-found">
+        <h2>포스트가 없어요..</h2>
+    </div>
+  );
+};
+
+export default NotFound;
