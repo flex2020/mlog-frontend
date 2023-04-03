8755ef25e9c6ebd62e2675df57520b03bf4ee7fc
NOH UIBIN <hosan_1@naver.com>
2023-04-03 22:29:18 +0900

    feat: 포스트 작성 구현 (파일 업로드 제외)

--- src/pages/Post/PostWrite.jsx
+++ src/pages/Post/PostWrite.jsx
@@ -1,31 +1,13 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import MDEditor from '../../components/InputField/MDEditor';
 import '../../assets/pages/Post/PostWrite.css'
-import TitleInput from '../../components/InputField/TitleInput';
-import CategorySelect from '../../components/InputField/CategorySelect';
-import DefaultButton from '../../components/Button/DefaultButton';
 
 const PostWrite = () => {
-  const navigate = useNavigate();
 
   return (
     <div className='container'>
       <div className='edtior-container'>
-        <TitleInput />
-        <CategorySelect />
         <MDEditor />
-      </div>
-      <div className='btn-container'>
-        <DefaultButton text='나가기' 
-        onClickHandler={() => {
-          navigate('/post');
-        }
-        } />
-        <DefaultButton text='포스트 저장'
-        onClickHandler={() => {
-          
-        }} />
       </div>
     </div>
   );
