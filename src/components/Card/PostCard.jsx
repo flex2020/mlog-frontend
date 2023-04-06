9cc91cf1222084998577a28a80195451fc7a000c
NOH UIBIN <hosan_1@naver.com>
2023-04-06 23:04:56 +0900

    update: 썸네일 경로 추가

--- src/components/Card/PostCard.jsx
+++ src/components/Card/PostCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import '../../assets/components/Card/PostCard.css'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
 
 const PostCard = ( {id, title, date, preview, thumbnail, replyCount} ) => {
+  const link = `/post/${id}`;
   return (
-    <div id={id} className='postcard'>
-      <img src={`/files/original/${thumbnail}`} alt={title} />
-      <div className='card-title'>{title}</div>
-      <div className='card-body'>{preview}</div>
-      <div className='card-bottom'>
-        <span>{replyCount}개의 댓글</span>
-        <span>작성 날짜: {date}</span>
+    <StyledLink to={link}>
+      <div id={id} className='postcard'>
+        <img src={`/files/original/${thumbnail}`} alt={title} />
+        <div className='card-title'>{title}</div>
+        <div className='card-body'>{preview}</div>
+        <div className='card-bottom'>
+          <span>{replyCount}개의 댓글</span>
+          <span>작성 날짜: {date}</span>
+        </div>
       </div>
-    </div>
+    </StyledLink>
   );
 };
 
