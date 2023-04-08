a8d9855a23f23a7df9c416ea8389aa6bebf29e08
NOH UIBIN <hosan_1@naver.com>
2023-04-08 12:11:40 +0900

    feat: 포스트 작성 화면 일부구현

--- src/pages/Post/PostView.jsx
+++ src/pages/Post/PostView.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import MDEditor from '@uiw/react-md-editor';
 import '../../assets/pages/Post/PostView.css';
+import Reply from '../../components/InputField/Reply';
 
 const PostView = () => {
   const { id } = useParams();
@@ -12,7 +13,10 @@ const PostView = () => {
   useEffect(() => {
     const getPost = async () => {
       const { data } = await axios.get(`${POST_API}/${id}`);
-      setPost(data);
+      const date = new Date(data.postedDate);
+      console.log(date);
+      const pdate = `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일 ${date.getHours()}:${date.getMinutes()}`;
+      setPost({...data, postedDate: pdate});
     };
     getPost();
   }, [id]);
@@ -22,10 +26,23 @@ const PostView = () => {
   return (
     <div className='view-background'>
       <div className='viewer-container'>
-      <h1 className='view-title'>{post.title}</h1>
+        <h1 className='view-title'>{post.title}</h1>
+        <div className='post-data'>
+          <span className='writer'>flex2020</span>
+          <span className='dot'>·</span>
+          <span className='date'>{post.postedDate}</span>
+        </div>
+        <div className='category-container'>
+          <span className='category'>{post.category}</span>
+        </div>
         <MDEditor.Markdown 
         className='viewer'
         source={post.content} />
+        <hr />
+        <div className='reply-container'>
+          <span className='reply-count'>{post.replyList.length}개의 댓글</span>
+          <Reply />
+        </div>
       </div>
     </div>
   );
