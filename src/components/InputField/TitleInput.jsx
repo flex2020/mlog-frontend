ab8b8c174981c157df69b3fcc8056a74d7b2341f
NOH UIBIN <hosan_1@naver.com>
2023-08-06 16:06:00 +0900

    delete: 디자인 변경을 위한 삭제

--- src/components/InputField/TitleInput.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-const TitleInput = ( { title, changeTitleHandler } ) => {
-  return (
-    <input className='title-input' type='text' value={title} onChange={changeTitleHandler} placeholder='제목을 입력해주세요.' />
-  );
-};
-
-export default TitleInput;
