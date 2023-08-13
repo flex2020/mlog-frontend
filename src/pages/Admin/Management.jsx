d1528c2c5ad9acf5c40bde966ad153d537bf4909
NOH UIBIN <hosan_1@naver.com>
2023-08-14 01:00:07 +0900

    create: 관리 페이지 생성

--- src/pages/Admin/Management.jsx
+++ src/pages/Admin/Management.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Page from '../../components/Page'
+import Header from '../../components/Header';
+import styled from 'styled-components';
+import ManagementTable from '../../components/ManagementTable';
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Title = styled.p`
+  font-size: 36px;
+  font-weight: 600;
+`;
 
 const Management = () => {
   return (
-    <div>
-      
-    </div>
+    <Page>
+      <Header showProfile={false} />
+      <TitleContainer>
+        <Title>통합 관리</Title>
+      </TitleContainer>
+      <ManagementTable />
+    </Page>
   );
 };
 
