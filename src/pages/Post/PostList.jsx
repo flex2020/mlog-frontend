fb19f1f1b0f42e13d6a79dfe1417187662982d74
NOH UIBIN <hosan_1@naver.com>
2023-10-26 16:47:46 +0900

    feat: 시리즈별로 보기 구현

--- src/pages/Post/PostList.jsx
+++ src/pages/Post/PostList.jsx
@@ -18,6 +18,41 @@ const Title = styled.p`
   font-weight: 600;
 `;
 
+const SeriesContainer = styled.div`
+  display: flex;
+  margin: 0 auto 40px;
+  width: calc(100% - 520px);
+  justify-content: center;
+  @media screen and (max-width: 500px) {
+    width: 100%;
+  }
+`;
+const SeriesCard = styled.div`
+  background-color: white;
+  color: black;
+  padding: 10px 15px;
+  margin: 10px 20px 0 0;
+  border-radius: 15px;
+  border: 1px solid #939393;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    background-color: #939393;
+    color: white;
+  }
+`;
+
+const SelectedSeriesCard = styled.div`
+  background-color: #939393;
+  color: white;
+  padding: 10px 15px;
+  margin: 10px 20px 0 0;
+  border-radius: 15px;
+  border: 1px solid #939393;
+  transition: 0.2s;
+  cursor: pointer;
+`;
+
 const SequenceButtonContainer = styled.div`
   display: flex;
   margin: 0 auto 40px;
@@ -76,25 +111,48 @@ const NoDataMsg = styled.p`
 const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [sequence, setSequence] = useState(0);
+  const [sereisList, setSeriesList] = useState([]);
+  const [currentSeries, setCurrentSeries] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
 
   const sequenceClick = () => {
     if (sequence === 0) setSequence(1);
     else setSequence(0);
     posts.reverse();
-
   }
+  const seriesClick = (e) => {
+    const newPosts = allPosts.filter((post) => post.series === e.target.getAttribute('data-value'));
+    setCurrentSeries(e.target.getAttribute('data-value'));
+    setPosts(newPosts);
+  }
+  const seriesClickCancel = (e) => {
+    setPosts(allPosts);
+    setSequence(0);
+    setCurrentSeries('');
+  }
+
 
   useEffect(() => {
     axios.get('/api/post')
     .then(( {data} ) => {
       if (Array.isArray(data)) {
         setPosts(data);
+        setAllPosts(data);
       }
     })
     .catch((error) => {
       console.log(error);
     })
-  }, [])
+    axios.get('/api/post/series')
+    .then(( {data} ) => {
+      if (Array.isArray(data)) {
+        setSeriesList(data);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, []);
 
 
   return (
@@ -120,6 +178,20 @@ const PostList = () => {
           <OldestButton onClick={sequenceClick} disabled>오래된 순</OldestButton>
         </SequenceButtonContainer>
       }
+      <SeriesContainer>
+        {
+          sereisList.map((series) => {
+            if (series.series === currentSeries) 
+              return (
+                <SelectedSeriesCard onClick={seriesClickCancel} data-value={series.series}>{series.series}</SelectedSeriesCard>
+              )
+            else
+              return (
+                <SeriesCard onClick={seriesClick} data-value={series.series}>{series.series}</SeriesCard>
+              )
+          })
+        }
+      </SeriesContainer>
       
       <CardContainer>
         {
